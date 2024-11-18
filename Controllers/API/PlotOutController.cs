323c73aafc04a57333afe91caa46649a598d1434
Eleonora Petzold <eleonora.petzold@uni-jena.de>
2024-11-18 15:16:40 +0100

    V3: Add new Api from 2.18 #150

--- Controllers/API/PlotOutController.cs
+++ Controllers/API/PlotOutController.cs
@@ -1,10 +1,16 @@
 ﻿using BExIS.App.Bootstrap.Attributes;
+using BExIS.Modules.Pmm.UI.Helper;
+using BExIS.Modules.Pmm.UI.Models;
 using BExIS.Security.Entities.Subjects;
 using BExIS.Security.Services.Authorization;
 using BExIS.Security.Services.Objects;
 using BExIS.Security.Services.Subjects;
 using BExIS.Utils.Route;
+using Microsoft.Web.Helpers;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,6 +18,8 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Web.Http;
 using Vaiona.Persistence.Api;
+using Vaiona.Web.Mvc.Modularity;
+
 
 namespace BExIS.Modules.PMM.UI.Controllers
 {
@@ -26,12 +34,27 @@ namespace BExIS.Modules.PMM.UI.Controllers
         /// <returns>List of plots with geometry</returns>
         [BExISApiAuthorize]
         [GetRoute("api/Plot")]
-      //  [HttpGet]
+        //  [HttpGet]
         public HttpResponseMessage Get()
         {
             string token = this.Request.Headers.Authorization?.Parameter;
 
             return getData(token, "plot", "");
+        }
+
+        // GET: api/Plot
+        /// <summary>
+        /// Get a list of all plots
+        /// </summary>
+        /// <returns>List of plots with geometry</returns>
+        [BExISApiAuthorize]
+        [GetRoute("api/PlotWithType")]
+        //  [HttpGet]
+        public HttpResponseMessage GetPlotsWithType()
+        {
+            string token = this.Request.Headers.Authorization?.Parameter;
+
+            return getData(token, "plots_withtype", "");
         }
 
 
@@ -80,12 +103,14 @@ namespace BExIS.Modules.PMM.UI.Controllers
         }
 
 
-        private HttpResponseMessage getData( string token, string type, string id)
+
+
+        private HttpResponseMessage getData(string token, string type, string id)
         {
 
             using (UserManager userManager = new UserManager())
             {
-             
+
                 // check token
                 if (String.IsNullOrEmpty(token))
                 {
@@ -94,14 +119,14 @@ namespace BExIS.Modules.PMM.UI.Controllers
 
                     return request;
                 }
-                
+
 
                 // get user from token
                 User user = ControllerContext.RouteData.Values["user"] as User;
 
                 if (user != null)
                 {
-                    
+
                     // check permission fror user
                     if (checkPermission(new Tuple<string, string, string>("PMM", "Main", "*"), user))
                     {
@@ -120,7 +145,13 @@ namespace BExIS.Modules.PMM.UI.Controllers
                         {
                             data = getSubplotsGrouped(id);
                         }
-                        
+                        else if (type == "plots_withtype")
+                        {
+                            var tempData = getPlots();
+                            data = getPlotsWithType(tempData);
+
+                        }
+
 
                         // return result as JSON
                         var response = Request.CreateResponse(HttpStatusCode.OK);
@@ -153,7 +184,7 @@ namespace BExIS.Modules.PMM.UI.Controllers
         {
             StringBuilder mvBuilder = new StringBuilder();
             mvBuilder.AppendLine(string.Format("SELECT jsonb_build_object( 'type', 'FeatureCollection', 'features', jsonb_agg(features.feature)) FROM ( SELECT jsonb_build_object('type', 'Feature', 'id', id, 'geometry', ST_AsGeoJSON(geometrytext::geometry)::jsonb,'properties', to_jsonb(inputs) - 'id' - 'geometrytext') AS feature FROM(SELECT * FROM pmm_plot) inputs) features; "));
-            
+
 
             // execute the statement
             try
@@ -169,6 +200,89 @@ namespace BExIS.Modules.PMM.UI.Controllers
                 return "";
             }
         }
+
+        private string getPlotsWithType(string plotResult)
+        {
+            Plots plots = JsonConvert.DeserializeObject<Plots>(plotResult);
+
+            var settings = ModuleManager.GetModuleSettings("pmm");
+
+            //get ep ref data set to find mips and vips
+            string epRefDatasetId = settings.GetValueByKey("epRefDataset").ToString();
+            var datasetObject = DataAccess.GetDatasetInfo(epRefDatasetId, GetServerInformation());
+            DataTable epPlotRefTable = DataAccess.GetData(epRefDatasetId, long.Parse(datasetObject.DataStructureId), GetServerInformation());
+
+            //get new experiment plots datasets
+            string foxRefDatasetId = settings.GetValueByKey("foxRefDataset").ToString();
+            var datasetObjectFox = DataAccess.GetDatasetInfo(foxRefDatasetId, GetServerInformation());
+            DataTable foxPlotRefTable = DataAccess.GetData(foxRefDatasetId, long.Parse(datasetObjectFox.DataStructureId), GetServerInformation());
+
+            string gNewExpRefDatasetId = settings.GetValueByKey("gNewExpDataset").ToString();
+            var datasetObjectgNewExp = DataAccess.GetDatasetInfo(gNewExpRefDatasetId, GetServerInformation());
+            DataTable gNewExpPlotRefTable = DataAccess.GetData(gNewExpRefDatasetId, long.Parse(datasetObjectgNewExp.DataStructureId), GetServerInformation());
+
+            //create lists with new experiment plot ids
+            List<string> foxPlots = foxPlotRefTable.AsEnumerable().Select(a => a.Field<string>("Joint Experiment ID")).ToList();
+            List<string> glnewExpPlots = gNewExpPlotRefTable.AsEnumerable().Select(a => a.Field<string>("EP ID")).ToList();
+            List<string> epPlots = epPlotRefTable.AsEnumerable().Select(a => a.Field<string>("EP_PlotID")).ToList();
+
+            foreach (var plot in plots.features)
+            {
+                if (epPlots.Contains(plot.properties.plotid))
+                {
+                    DataRow row = epPlotRefTable.AsEnumerable().Where(a => a.Field<string>("EP_PlotID") == plot.properties.plotid).FirstOrDefault();
+
+                    if (row.Field<string>("VIP") == "yes")
+                        plot.properties.plotType = "VIP";
+
+                    if (row.Field<string>("MIP") == "yes")
+                        plot.properties.plotType = "MIP";
+                    else
+                        plot.properties.plotType = "EP";
+
+                }
+                if (foxPlots.Contains(plot.properties.plotid))
+                {
+                    plot.properties.plotType = "Fox";
+                }
+                if (glnewExpPlots.Contains(plot.properties.plotid))
+                {
+                    DataRow row = gNewExpPlotRefTable.AsEnumerable().Where(a => a.Field<string>("EP ID") == plot.properties.plotid).FirstOrDefault();
+                    if (row.Field<string>("REX I") == "yes")
+                        plot.properties.plotType += ", REX I";
+                    if (row.Field<string>("REX II") == "yes")
+                        plot.properties.plotType += ", REX II";
+                    if (row.Field<string>("LUX") == "yes")
+                        plot.properties.plotType += ", LUX";
+                }
+
+                if (plot.properties.plotid.Contains("EG"))
+                {
+                    plot.properties.habitat = "Grassland";
+                }
+                else
+                    plot.properties.habitat = "Forest";
+            }
+
+            return JsonConvert.SerializeObject(plots);
+        }
+
+        /// <summary>
+        /// Get server information form json file in workspace
+        /// </summary>
+        /// <returns></returns>
+        private ServerInformation GetServerInformation()
+        {
+            ServerInformation serverInformation = new ServerInformation();
+            var uri = System.Web.HttpContext.Current.Request.Url;
+            //serverInformation.ServerName = "http://be2020-dev.inf-bb.uni-jena.de:2010/";
+            serverInformation.Token = this.Request.Headers.Authorization?.Parameter;
+            serverInformation.ServerName = uri.GetLeftPart(UriPartial.Authority) + "/";
+            //serverInformation.Token = GetUserToken();
+
+            return serverInformation;
+        }
+
 
         // get list of subplots (all or for one explo)
         private string getSubplots(string id)
@@ -187,17 +301,17 @@ namespace BExIS.Modules.PMM.UI.Controllers
 
             // execute the statement
             try
+            {
+                using (IUnitOfWork uow = this.GetBulkUnitOfWork())
                 {
-                    using (IUnitOfWork uow = this.GetBulkUnitOfWork())
-                    {
-                        var result = uow.ExecuteScalar(mvBuilder.ToString());
-                        return (string)result;
-                    }
+                    var result = uow.ExecuteScalar(mvBuilder.ToString());
+                    return (string)result;
                 }
-                catch
-                {
-                    return "";
-                }
+            }
+            catch
+            {
+                return "";
+            }
 
         }
 
@@ -247,13 +361,13 @@ namespace BExIS.Modules.PMM.UI.Controllers
                 var controllerName = LandingPage.Item2;
                 var actionName = LandingPage.Item3;
 
-  
+
                 var operation = operationManager.Find(areaName, controllerName, "*");
 
                 var feature = operation.Feature;
                 if (feature == null) return true;
 
- 
+
 
                 if (featurePermissionManager.HasAccessAsync(user.Id, feature.Id).Result)
                 {
